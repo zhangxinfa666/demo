8ad4f5b145656c43d1eff1f4f63566b33bb2080d
zhangxinfa <666666@qq.com>
2018-07-18 16:51:03 +0800

    修改

--- src/GitDemo.java
+++ src/GitDemo.java
@@ -4,6 +4,8 @@ public class GitDemo {
 	public static void main(String[] args) {
 		System.out.println("第一个Git管理的项目");
 		System.out.println("我开始修改效能革命");
+		System.out.println("第一次修改");
+		System.out.println("465146w5w3tryty484ghdf53r465i23uhg46s5e3258765iu");
 	}
 
 }
